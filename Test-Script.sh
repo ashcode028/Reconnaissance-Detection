b9110c032249c2ee007aeac7c36f84de42d7f1ce
Ashita <cOde.A4sh@gmail.com>
2022-02-19 18:47:37 +0530

    Updated test script

--- Test-Script.sh
+++ Test-Script.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
     echo "ACK SCAN"
-    nmap -sA 172.16.12.131 &
+    nmap -sA 172.16.12.131 
     echo "XMAS SCAN"
-    nmap -sX 172.16.12.131 &
+    nmap -sX 172.16.12.131
     echo "FIN SCAN"
-    nmap -sF 172.16.12.131 &
+    nmap -sF 172.16.12.131
     echo "SYN SCAN"
-    nmap -sS 172.16.12.131 &
+    nmap -sS 172.16.12.131
     echo "NULL SCAN"
-    nmap -sN 172.16.12.131 &
+    nmap -sN 172.16.12.131 
 
 wait 
 echo "All scans done"
