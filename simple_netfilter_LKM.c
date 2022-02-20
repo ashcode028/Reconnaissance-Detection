e721ad97edffb890102e31a30d1d630fe31da49a
Ashita Boyina <55681942+ashcode028@users.noreply.github.com>
2022-02-20 19:26:32 +0530

    Update simple_netfilter_LKM.c

--- simple_netfilter_LKM.c
+++ simple_netfilter_LKM.c
@@ -14,22 +14,73 @@ static struct nf_hook_ops *nfho = NULL;
 static unsigned int hfunc(void *priv, struct sk_buff *skb, const struct nf_hook_state *state)
 {
     struct iphdr *iph;
-    struct udphdr *udph;
+    struct tcphdr *tcp_header;
     if (!skb)
         return NF_ACCEPT;
 
     iph = ip_hdr(skb);
-    if (iph->protocol == IPPROTO_UDP) {
-        udph = udp_hdr(skb);
-        if (ntohs(udph->dest) == 53) {
-            return NF_ACCEPT;
-        }
-    }
-    else if (iph->protocol == IPPROTO_TCP) {
-        return NF_ACCEPT;
+    if (iph->protocol == IPPROTO_TCP) {
+        printk(KERN_INFO "TCP packet detected!\n");
+
+                tcp_header = (struct tcphdr *) skb_transport_header(skb);
+
+                /**
+                 * NULL Scan
+                 */
+                if (tcp_header->syn == 0
+                    && tcp_header->ack == 0
+                    && tcp_header->urg == 0
+                    && tcp_header->rst == 0
+                    && tcp_header->fin == 0
+                    && tcp_header->psh == 0) {
+                        printk(KERN_INFO "NULL Scan detected!\n");
+                }
+
+
+                /**
+                 * ACK Scan
+                 */
+                else if (tcp_header->syn == 0
+                         && tcp_header->ack == 1
+                         && tcp_header->urg == 0
+                         && tcp_header->rst == 0
+                         && tcp_header->fin == 0
+                         && tcp_header->psh == 0) {
+
+                        printk(KERN_INFO "ACK Scan detected!\n");
+                }
+
+
+
+                /**
+                 * FIN Scan
+                 */
+                else if (tcp_header->syn == 0
+                         && tcp_header->ack == 0
+                         && tcp_header->urg == 0
+                         && tcp_header->rst == 0
+                         && tcp_header->fin == 1
+                         && tcp_header->psh == 0) {
+
+                        printk(KERN_INFO "FIN Scan detected!\n");
+                }
+
+
+                /**
+                 * XMAS Scan
+                 */
+                else if (tcp_header->syn == 0
+                         && tcp_header->ack == 0
+                         && tcp_header->urg == 1
+                         && tcp_header->rst == 0
+                         && tcp_header->fin == 1
+                         && tcp_header->psh == 1) {
+
+                        printk(KERN_INFO "XMAS Scan detected!\n");
+                }
     }
     
-    return NF_DROP;
+    return NF_ACCEPT;
 }
 
 static int __init LKM_init(void)
